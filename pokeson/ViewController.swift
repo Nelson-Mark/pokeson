0af5465a5d0227093e95257a3015518fb3f7dbd8
Mark Nelson <marknelson@Marks-MacBook-Pro.local>
2017-12-27 13:05:47 +0530

    Initial Commit

---
+++ pokeson/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokeson
+//
+//  Created by Mark Nelson on 27/12/17.
+//  Copyright © 2017 Mark Nelson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
